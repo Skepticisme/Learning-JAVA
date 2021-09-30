5694e5056439747cd7a9b4da91e66049bba3c33a
Skepticisme <73161831+Skepticisme@users.noreply.github.com>
2021-09-30 21:19:40 +0000

    Initial commit

---
+++ Bank.java
@@ -0,0 +1,28 @@
+public class Bank{
+	
+	//CUSTOMER SIDE VARIABLES 
+
+	
+
+	//BANK SIDE VARIABLES
+	String bank_name; 
+	
+	Bank(String name){ 
+		this.bank_name=name;
+	}
+ 
+	//Transaction:
+		//CHECK 
+		private int enough_balance(int money,Customer P)
+			return P.getBalance == money;
+			 
+
+
+
+
+
+	
+
+
+
+}
